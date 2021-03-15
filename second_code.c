cb1adc38abdf6ce24f39b38a038a369995073ab3
“abhisheksaurabh53” <abhishek.saurabh53@gmail.com>
2021-03-15 17:43:04 +0530

    Modified 2nd time

--- second_code.c
+++ second_code.c
@@ -2,16 +2,22 @@
 #include<string.h>
 int main()
 {
-    char c[30]="aaabbbcdde";
-    int count;int l=strnlen(c);printf("%d",l);
-    for(int i=0;i< l;i++)
+    char c[30]="aaabbbcddeaddtthzzzaa",a;
+    int count;int l=strlen(c);int i=0,j=0;
+    
 
-    {   count=1;//i=count-1;
-        for(int j=i+1;j<l;j++)
-       { if (c[i]==c[j]) count++;
+     
+        while(c[i]!='\0')
+        {
+        while(c[i]-c[i+1]==0) 
+        {i++;j++; } 
 
+         count=j+1;
+        printf("%c%d",c[i-j],count);
+        i++;
+        j=0;
+        
+    
         }
-        c[i+1]=(char)count;
-    }
     return 0;
 }
